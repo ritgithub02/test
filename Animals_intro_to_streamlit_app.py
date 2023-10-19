10fa550944ad378555e4e6b7b744579c1fbfd442
ritgithub02 <146735019+ritgithub02@users.noreply.github.com>
2023-10-19 23:50:35 +0530

    Animals_intro_to_streamlit_app.py

--- Animals_intro_to_streamlit_app.py
+++ Animals_intro_to_streamlit_app.py
@@ -1,41 +1,3 @@
-# import streamlit as st
-# import pandas as pd
-
-# # Create file uploader object
-# upload_file = st.file_uploader('Upload a file for prediction', type=['csv'])
-# rt0 = None  # Initialize rt0 as None
-
-# # Load the CSV file into a Pandas DataFrame
-# if upload_file is not None:
-#     rt0 = pd.read_csv(upload_file)
-#     rt0 = rt0.dropna()
-
-# st.title('Data')
-
-# # Initialize selected_columns to None
-# selected_columns = None
-
-# # Check if rt0 is not None (file is uploaded)
-# if rt0 is not None:
-#     selected_columns = st.multiselect('Select columns to display:', rt0.columns)
-#     rt = rt0[selected_columns] if selected_columns else rt0
-
-#     st.write(rt.describe())
-#     rt.to_csv('al.csv')
-
-# # Read the CSV file for further processing
-# ui = pd.read_csv('al.csv')
-
-# # Display the summary statistics of the loaded data
-# st.write(ui.describe())
-
-
-
-
-
-
-
-
 import numpy as np
 import pandas as pd
 import lasio 
@@ -116,7 +78,11 @@ ui = pd.read_csv('al.csv')
 # Display the summary statistics of the loaded data
 st.write(ui.describe())
 
-
+    well_data = pd.read_csv(upload_file)
+    well_df = well_data.df()
+    well_df=well_df.dropna()
+    well_df=well_df.reset_index()
+    columns= well_df.columns
 
 
 # ------------------------------------------------------------------------------------------
@@ -202,8 +168,6 @@ st.write(ui.describe())
 #     well_df=well_df.reset_index()
 #     columns= well_df.columns
 
-
-
 # # --------------------------------------------------------------------
 
 
@@ -246,33 +210,21 @@ st.write(ui.describe())
 
 
 
-# tab_a,tab_b, tab_c =st.tabs(['Data Loading', 'Formation Evaluation', 'Visualization' ])
+tab_a,tab_b, tab_c =st.tabs(['Data Loading', 'Formation Evaluation', 'Visualization' ])
 
-# with tab_a:
-#     st.subheader("Upload LAS File")
-#     uploaded_file = st.file_uploader("Upload a LAS file", type=["las", "LAS"])
-#     def display_las_data(uploaded_file):
-#         if uploaded_file is not None:
-#             las_data = lasio.read(uploaded_file)
-#             st.header("LAS Header")
-#             st.write(las_data.header)
-    
-#             st.header("LAS Data")
-#             st.write(las_data.df())
-    
-#     # Check if a file has been uploaded
-#     if uploaded_file:
-#         display_las_data(uploaded_file)
-#     else:
-#         st.info("Please upload a LAS file.")
+with tab_a:
+
+    well_df = pd.read_csv('al.csv')
+    st.write('Logs:')
+    st.write(well_df.columns)
+    st.write('Statistics:')
+    st.write(well_df.describe())    
+
 #     # st.write('Missing Ranges :')
 #     # # st.write(msno.matrix(well_df))
 #     # st.pyplot(msno.matrix(well_df))
     
-#     st.write('Logs:')
-#     st.write(well_df.columns)
-#     st.write('Statistics:')
-#     st.write(well_df.describe())
+
 
 
 
