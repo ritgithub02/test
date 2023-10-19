4f978c9f6be4773c9cb9c7bd394373902e0506f1
ritgithub02 <146735019+ritgithub02@users.noreply.github.com>
2023-10-19 23:30:21 +0530

    Animals_intro_to_streamlit_app.py

--- Animals_intro_to_streamlit_app.py
+++ Animals_intro_to_streamlit_app.py
@@ -1,7 +1,5 @@
-import pandas as pd
 import streamlit as st
-
-
+import pandas as pd
 
 # Create file uploader object
 upload_file = st.file_uploader('Upload a file for prediction', type=['csv'])
@@ -10,13 +8,12 @@ rt0 = None  # Initialize rt0 as None
 # Load the CSV file into a Pandas DataFrame
 if upload_file is not None:
     rt0 = pd.read_csv(upload_file)
-    rt0=rt0.dropna()
+    rt0 = rt0.dropna()
 
 st.title('Data')
 
-# Initialize selected_columns and selected_t to None
+# Initialize selected_columns to None
 selected_columns = None
-selected_t = None
 
 # Check if rt0 is not None (file is uploaded)
 if rt0 is not None:
@@ -26,6 +23,8 @@ if rt0 is not None:
     st.write(rt.describe())
     rt.to_csv('al.csv')
 
-ui=pd.read_csv('al.csv')
-ui=df()
-ui.describe()
+# Read the CSV file for further processing
+ui = pd.read_csv('al.csv')
+
+# Display the summary statistics of the loaded data
+st.write(ui.describe())
