575c2bddddb759d05f42bffa4a7d414839a42a15
ritgithub02 <146735019+ritgithub02@users.noreply.github.com>
2023-10-20 00:26:46 +0530

    Animals_intro_to_streamlit_app.py

--- Animals_intro_to_streamlit_app.py
+++ Animals_intro_to_streamlit_app.py
@@ -200,6 +200,72 @@ import missingno as msno
 
 ### ------------------------------------------------------------------------------------
 # Define the load_data function
+# def load_data(uploadedfile):
+#     if uploadedfile:
+#         uploadedfile.seek(0)  # Reset buffer to the beginning each time
+#         string = uploadedfile.read().decode()
+#         las_file = lasio.read(string)
+
+#         # Create the DataFrame
+#         well_data = las_file.df()
+
+#         # Assign the DataFrame index to a curve
+#         # well_data['DEPTH'] = well_data.index.astype(bool)
+#     else:
+#         las_file = None
+#         well_data = None
+
+#     return las_file, well_data
+
+# # Create a Streamlit app
+# def main():
+#     st.title("LAS File Viewer")
+
+#     # Upload a LAS file
+#     uploaded_file = st.file_uploader("Upload a LAS file", type=["las", "LAS"])
+
+#     if uploaded_file is not None:
+#         st.write("File Details:")
+#         st.write("Name:", uploaded_file.name)
+#         st.write("Type:", uploaded_file.type)
+#         st.write("Size:", uploaded_file.size, "bytes")
+
+#         # Call the load_data function
+#         las_file, well_data = load_data(uploaded_file)
+
+#         if las_file is not None:
+#             st.success("LAS file loaded successfully")
+#             st.write("Well Data:")
+#             # st.write(well_data)
+
+#     # Return well_data from the main function
+#     return well_data
+
+# if __name__ == "__main__":
+#     well_data = main()
+#     if well_data is not None:
+#         well_data.reset_index(inplace=True)
+#         st.write(well_data)
+#         st.write(well_data.describe())
+#         st.write(well_data['DTCO'])
+#         st.write(well_df=well_data.copy())
+#         well_data.to_csv('al.csv',index=False)
+### --------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+import streamlit as st
+import lasio
+
 def load_data(uploadedfile):
     if uploadedfile:
         uploadedfile.seek(0)  # Reset buffer to the beginning each time
@@ -223,6 +289,8 @@ def main():
 
     # Upload a LAS file
     uploaded_file = st.file_uploader("Upload a LAS file", type=["las", "LAS"])
+
+    las_file, well_data = None, None  # Set default values
 
     if uploaded_file is not None:
         st.write("File Details:")
@@ -249,8 +317,16 @@ if __name__ == "__main__":
         st.write(well_data.describe())
         st.write(well_data['DTCO'])
         st.write(well_df=well_data.copy())
-        well_data.to_csv('al.csv',index=False)
-### --------------------------------------------------------------------------------------------------
+        well_data.to_csv('al.csv', index=False)
+
+
+
+
+
+
+
+
+
 
 
 
