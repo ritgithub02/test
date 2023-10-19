756d962cca6394783dd27cdcfdbe6c5a126afc90
ritgithub02 <146735019+ritgithub02@users.noreply.github.com>
2023-10-19 19:42:45 +0530

    Animals_intro_to_streamlit_app.py

--- Animals_intro_to_streamlit_app.py
+++ Animals_intro_to_streamlit_app.py
@@ -7,6 +7,11 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 
+
+
+
+
+
 # Define the load_data function
 def load_data(uploadedfile):
     if uploadedfile:
@@ -18,7 +23,7 @@ def load_data(uploadedfile):
         well_data = las_file.df()
 
         # Assign the DataFrame index to a curve
-        well_data['DEPTH'] = well_data.index
+        well_data['DEPTH'] = well_data.index.astype(bool)
     else:
         las_file = None
         well_data = None
@@ -44,63 +49,10 @@ def main():
         if las_file is not None:
             st.success("LAS file loaded successfully.")
             st.write("Well Data:")
-            st.code(well_data=well_data.reset_index(Inplace=True))
             st.write(well_data)
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-# # Define the load_data function
-# def load_data(uploadedfile):
-#     if uploadedfile:
-#         uploadedfile.seek(0)  # Reset buffer to the beginning each time
-#         string = uploadedfile.read().decode()
-#         las_file = lasio.read(string)
-
-#         # Create the DataFrame
-#         well_data = las_file.df()
-
-#         # Assign the DataFrame index to a curve
-#         well_data['DEPTH'] = well_data.index.astype(bool)
-#     else:
-#         las_file = None
-#         well_data = None
-
-#     return las_file, well_data
-
-# # Create a Streamlit app
-# def main():
-#     st.title("LAS File Viewer")
-
-#     # Upload a LAS file
-#     uploaded_file = st.file_uploader("Upload a LAS file", type=["las", "LAS"])
-
-#     if uploaded_file is not None:
-#         st.write("File Details:")
-#         st.write("Name:", uploaded_file.name)
-#         st.write("Type:", uploaded_file.type)
-#         st.write("Size:", uploaded_file.size, "bytes")
-
-#         # Call the load_data function
-#         las_file, well_data = load_data(uploaded_file)
-
-#         if las_file is not None:
-#             st.success("LAS file loaded successfully.")
-#             st.write("Well Data:")
-#             st.write(well_data)
-
-# if __name__ == "__main__":
-#     main()
 
 
 
