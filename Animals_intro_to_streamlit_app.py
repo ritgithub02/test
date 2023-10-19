6d4cba38983f6a366305ffb242f7736bfdbd912d
ritgithub02 <146735019+ritgithub02@users.noreply.github.com>
2023-10-20 00:37:32 +0530

    Animals_intro_to_streamlit_app.py

--- Animals_intro_to_streamlit_app.py
+++ Animals_intro_to_streamlit_app.py
@@ -1,21 +1,8 @@
-import numpy as np
-import pandas as pd
-import lasio 
-import matplotlib.pyplot as plt
+
 import streamlit as st
 
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
-import plotly.express as px
-import plotly.subplots
-from PIL import Image
-import missingno as msno
 
 
-
-
-
-# ----------------------------------
 
 tab_a,tab_b, tab_c =st.tabs(['Data Loading', 'Formation Evaluation', 'Visualization' ])
 
@@ -27,30 +14,6 @@ with tab_a:
 
 with tab_b:
     st.title("Vshale Plot")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
