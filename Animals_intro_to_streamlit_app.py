e63d85d2019d2bcfc34f787f3404f118df2bab46
ritgithub02 <146735019+ritgithub02@users.noreply.github.com>
2023-10-19 21:46:35 +0530

    Animals_intro_to_streamlit_app.py

--- Animals_intro_to_streamlit_app.py
+++ Animals_intro_to_streamlit_app.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
         st.write(well_data)
         st.write(well_data.describe())
         st.write(well_data['DTCO'])
-        well_df=well_data.copy()
+        st.write(well_df=well_data.copy())
 
 
 
