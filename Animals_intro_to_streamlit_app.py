69db97df4cdb2f3ba7da33e2cea2d7979d37cdc3
ritgithub02 <146735019+ritgithub02@users.noreply.github.com>
2023-10-20 15:58:53 +0530

    Animals_intro_to_streamlit_app.py

--- Animals_intro_to_streamlit_app.py
+++ Animals_intro_to_streamlit_app.py
@@ -72,24 +72,24 @@ with t1:
         # Return well_data from the main function
         return well_data
     
-    if __name__ == "__main__":
-        well_data = main()
-        if well_data is not None:
-            well_data.reset_index(inplace=True)
-            well_df = pd.DataFrame(well_data)
-            columns=well_df.columns
-            st.write("Well Data:")
-            st.write(well_df)
-            st.write("Statistics:")
-            st.write(well_df.describe())
+    # if __name__ == "__main__":
+    #     well_data = main()
+    #     if well_data is not None:
+    #         well_data.reset_index(inplace=True)
+    #         well_df = pd.DataFrame(well_data)
+    #         columns=well_df.columns
+    #         st.write("Well Data:")
+    #         st.write(well_df)
+    #         st.write("Statistics:")
+    #         st.write(well_df.describe())
 
-    #         well_data.to_csv('io.csv')
+    # #         well_data.to_csv('io.csv')
 
 
-    # well_df= pd.read_csv('io.csv')
-    # columns=well_df.columns
-    else:
-        st.write("Well data is None")
+    # # well_df= pd.read_csv('io.csv')
+    # # columns=well_df.columns
+    # else:
+    #     st.write("Well data is None")
     
 
 
